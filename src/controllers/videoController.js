70efa1849ddde5edfad2d737d938c9b836d95f13
Gyeong a Kim <alwayz0121@gmail.com>
2022-08-28 17:59:26 +0900

    정규식을 이용해 숫자 params만 거르기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,14 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("Watch Videos");
-export const edit = (req, res) => res.send("Edit Videos");
+export const see = (req, res) => {
+  return res.send(`See Video #${req.params.id}`);
+};
+export const edit = (req, res) => {
+  console.log(req.params);
+  return res.send("Edit Video");
+};
+export const search = (req, res) => res.send("Search Video");
+export const upload = (req, res) => res.send("Upload Video");
+export const remove = (req, res) => {
+  console.log(req.params);
+  return res.send("Remove Video");
+};
